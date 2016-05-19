21a27e61cd0d79745f0d6fd363bb6c79ecdf601f
root <root@ip-172-31-14-151.ap-northeast-2.compute.internal>
2016-05-19 14:40:27 +0000

    test query builder

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -2,10 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api extends CI_Controller {
-	public function info()
-	{
-		$this->load->view('test');
-	}
 
 	public function db_connect()
 	{
@@ -24,15 +20,15 @@ class Api extends CI_Controller {
 			->set_output(json_encode($data, JSON_UNESCAPED_UNICODE));
 	}
 	
-	public function get_favorite_city_departures()
+	public function get_city_favorite_departures()
 	{
 	}
 	
 	public function get_city_departures()
 	{
 		$this->db_connect();
-
 		$response_data = array();
+		
 		$query = $this->db->get_where('stop_info', array('division' => 001));
 
 		foreach ($query->result() as $row)
@@ -45,7 +41,11 @@ class Api extends CI_Controller {
 		$this->db_disconnect();
 	}
 
-	public function get_city_destinations($departure)
+	public function get_city_favorite_destinations()
+	{
+	}
+
+	public function get_city_destinations($departure_id)
 	{
 	}
 
@@ -58,15 +58,44 @@ class Api extends CI_Controller {
 
 		foreach ($query->result() as $row)
 		{
-			array_push($response_data, array('id' => $row->id, 'name' => $row->name));
+			array_push($response_data, array('id' => $row->id,
+							'name' => $row->name));
 		}
 		
 		$this->output_json_format($response_data);
 
 		$this->db_disconnect();
 	}
+	
+	public function get_intercity_favorite_destinations()
+	{
+	}
+	
+	public function get_route($departure_id, $destination_id)
+	{
+		$this->db_connect();
+		
+		$response_data = array();
 
-	public function get_stops_in_route()
+		$this->db->select('a.id as id');
+		$this->db->from('routes as a, stop_info as b, stop_info c');
+		$this->db->where('a.departure = b.id');
+		$this->db->where('a.destination = c.id');
+		$this->db->where('a.departure = ', $departure_id);
+		$this->db->where('a.destination = ', $destination_id);
+		$query = $this->db->get();
+
+		foreach ($query->result() as $row)
+		{
+			array_push($response_data, array('id' => $row->id));
+		}
+
+		$this->output_json_format($response_data);
+
+		$this->db_disconnect();
+	}
+
+	public function get_stops_in_route($route_id)
 	{
 	}
 }
