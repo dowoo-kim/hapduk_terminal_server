b6f7a55210ab02690a583d53280259b5ef12c322
root <root@ip-172-31-14-151.ap-northeast-2.compute.internal>
2016-05-26 18:22:23 +0000

    modified functions

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -26,6 +26,7 @@ class Api extends CI_Controller {
 	
 	public function get_city_departures()
 	{
+		/*
 		$this->db_connect();
 		$response_data = array();
 		
@@ -39,13 +40,14 @@ class Api extends CI_Controller {
 		$this->output_json_format($response_data);
 
 		$this->db_disconnect();
+		*/
 	}
 
 	public function get_city_favorite_destinations()
 	{
 	}
 
-	public function get_city_destinations($departure_id)
+	public function get_city_destinations()
 	{
 	}
 
@@ -55,55 +57,25 @@ class Api extends CI_Controller {
 
 		$response_data = array();
 		
-		$this->db->select(array('a.id as stop_info_id',
-					'a.name as stop_info_name',
+		$this->db->select(array('a.id as destination_id',
+					'a.name as destination_name',
 					'b.id as division_id',
 					'b.name as division_name'));
 		$this->db->from(array('stop_info as a',
 					'division as b'));
+		$this->db->where('a.division not in (001,000)');
 		$this->db->where('a.division = b.id');
-		$this->db->where('a.division != 001');
 
 		$query = $this->db->get();
 
 		foreach ($query->result() as $row)
 		{
-			array_push($response_data, array('stop_info_id' => $row->stop_info_id,
-							'stop_info_name' => $row->stop_info_name,
-							'division_id' => $row->division_id,
-							'division_name' => $row->division_name));
+			array_push($response_data, array('destinationId' => $row->destination_id,
+							'destinationName' => $row->destination_name,
+							'divisionId' => $row->division_id,
+							'divisionName' => $row->division_name));
 		}
 		
-		$this->output_json_format($response_data);
-
-		$this->db_disconnect();
-	}
-
-	public function get_intercity_destinations_to_division($division_id)
-	{
-		$this->db_connect();
-
-		$response_data = array();
-
-		$this->db->select(array('a.id as stop_info_id',
-					'a.name as stop_info_name',
-					'b.id as division_id',
-					'b.name as division_name'));
-		$this->db->from(array('stop_info as a',
-					'division as b'));
-		$this->db->where('a.division = b.id');
-		$this->db->where('a.division =',$division_id);
-
-		$query = $this->db->get();
-
-		foreach ($query->result() as $row)
-		{
-			array_push($response_data, array('stop_info_id' => $row->stop_info_id,
-							'stop_info_name' => $row->stop_info_name,
-							'division_id' => $row->division_id,
-							'division_name' => $row->division_name));
-		}
-
 		$this->output_json_format($response_data);
 
 		$this->db_disconnect();
@@ -113,127 +85,87 @@ class Api extends CI_Controller {
 	{
 	}
 
-	public function get_route_id($departure_id, $destination_id)
+	public function get_route($destination_id)
 	{
-		$this->db_connection();
+		$this->db_connect();
 
-		$response_data = array();
+		$route_ids = array();
 
 		$this->db->select('id');
 		$this->db->from('routes');
-		$this->db->where('departure = ', $departure_id);
 		$this->db->where('destination = ', $destination_id);
-
-		$query = $this->db->get();
-
-		foreach($query->result() as $row)
-		{
-			array_push($response_data, array('route_id' => $row->id));
-		}
-
-		$this->output_json_format($response_data);
-
-		$this->db_disconnect();
-	}
-	
-	public function get_route($route_id)
-	{
-		$this->db_connect();
-		
-		$response_data = array();
-
-		$this->db->select(array('a.id as route_id',
-					'a.departure as departure_id',
-					'b.name as departure_name',
-					'a.destination as destination_id',
-					'c.name as destination_name',
-					'a.required as required'));
-		$this->db->from(array('routes as a',
-					'stop_info as b',
-					'stop_info as c'));
-		$this->db->where('a.departure = b.id');
-		$this->db->where('a.destination = c.id');
-		$this->db->where('a.id = ', $route_id);
 		
 		$query = $this->db->get();
-
-		foreach ($query->result() as $row)
+		
+		if($query->num_rows() > 0)
 		{
-			array_push($response_data, array('route_id' => $row->route_id,
-							'departure_id' => $row->departure_id,
-							'departure_name' => $row->departure_name,
-							'destination_id' => $row->destination_id,
-							'destination_name' => $row->destination_name,
-							'required' => $row->required));
+			foreach($query->result() as $row)
+			{
+				if(!in_array($row->id, $route_ids))
+				{
+					array_push($route_ids, $row->id);
+				}
+			}
+		}
+		
+		$this->db->select('route_id');
+		$this->db->from('stops_in_route');
+		$this->db->where('stop_info_id = ', $destination_id);
+
+		$query = $this->db->get();
+		
+		if($query->num_rows() > 0)
+		{
+			foreach($query->result() as $row)
+			{
+				if(!in_array($row->route_id, $route_ids))
+				{
+					array_push($route_ids, $row->route_id);
+				}
+			}
 		}
 
-		$this->output_json_format($response_data);
-
-		$this->db_disconnect();
-	}
-
-	public function get_stops_in_route($route_id)
-	{
-		$this->db_connect();
-
-		$response_data = array();
-
-		$this->db->select(array('a.route_id as route_id',
+		$this->db->select(array('b.route_id as route_id',
+					'a.id as stops_in_route_id',
+					'a.sequence as stops_in_route_sequence',
+					'b.id as time_table_id',
 					'b.departure_time as departure_time',
-					'c.id as stop_id',
-					'c.name as stop_name',
-					'a.sequence as stops_in_route_sequence'));
+					'c.id as stop_info_id',
+					'c.name as stop_info_name',
+					'c.child as stop_info_child',
+					'c.teenager as stop_info_teenager',
+					'c.adult as stop_info_adult',
+					'c.required as stop_info_required'));
 		$this->db->from(array('stops_in_route as a',
 					'time_table as b',
 					'stop_info as c'));
-		$this->db->where('a.route_id = b.route_id');
+		$this->db->where_in('b.route_id', $route_ids);
+		$this->db->where('b.route_id = a.route_id');
 		$this->db->where('a.stop_info_id = c.id');
-		$this->db->where('a.route_id', $route_id);
-		$this->db->order_by('b.departure_time','ASC');
-		$this->db->order_by('a.sequence','ASC');
-		
+		$this->db->order_by('b.departure_time', 'ASC');
+		$this->db->order_by('b.route_id', 'ASC');
+		$this->db->order_by('a.sequence', 'ASC');
+
 		$query = $this->db->get();
 
-		foreach($query->result() as $row)
+		$stops_in_route = array();
+
+		foreach ($query->result() as $row)
 		{
-			array_push($response_data, array('route_id' => $row->route_id,
+			array_push($stops_in_route, array('id' => $row->stop_info_id,
+							'name' => $row->stop_info_name,
+							'route_id' => $row->route_id,
+							'stops_in_route_id' => $row->stops_in_route_id,
+							'sequence' => $row->stops_in_route_sequence,
+							'time_table_id' => $row->time_table_id,
 							'departure_time' => $row->departure_time,
-							'stop_id' => $row->stop_id,
-							'stop_name' => $row->stop_name,
-							'stops_in_route_sequence' => $row->stops_in_route_sequence));
+							'child' => $row->stop_info_child,
+							'teenager' => $row->stop_info_teenager,
+							'adult' => $row->stop_info_adult,
+							'required' => $row->stop_info_required));
 		}
 
-		$this->output_json_format($response_data);
-
-		$this->db_disconnect();
-	}
-
-	public function get_fee($stop_info_id)
-	{
-		$this->db_connect();
-
-		$response_data = array();
-
-		$this->db->select(array('id',
-					'name',
-					'child',
-					'teenager',
-					'adult'));
-		$this->db->from('stop_info');
-		$this->db->where('id', $stop_info_id);
-		
-		$query = $this->db->get();
-
-		foreach($query->result() as $row)
-		{
-			array_push($response_data, array('stop_info_id' => $row->id,
-							'stop_info_name' => $row->name,
-							'child' => $row->child,
-							'teenager' => $row->teenager,
-							'adult' => $row->adult));
-		}
-
-		$this->output_json_format($response_data);
+		$this->output_json_format($stops_in_route);
 
 		$this->db_disconnect();
 	}
